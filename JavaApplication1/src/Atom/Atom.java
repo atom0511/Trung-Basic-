a428bc769b7883354f552851726dc6eb5ed50331
Trung <totrungatom@gmail.com>
2019-08-14 21:01:11 +0700

    Lesson 1

---
+++ JavaApplication1/src/Atom/Atom.java
@@ -0,0 +1,27 @@
+
+package Atom;
+
+import java.util.Scanner;
+
+public class Atom {
+    
+//    public static void main(String[] args) {
+////        Giai pt ax + b = 0
+//     Scanner scanner = new Scanner(System.in);
+////        System.out.println("Nhap vao gia tri a dang int:");
+////     int a = scanner.nextInt();
+////        System.out.println("Nhap vao gia tri b dang float: ");
+////                
+////     float b = scanner.nextFloat();
+//        System.out.println("Lời giải bài toán: ");
+//     char c = scanner.next().charAt(9);
+//     
+////        System.out.println(" x = -b/a = " + -b/a);
+//        
+//     
+      public static void main(String[] args) {
+        System.out.printf("%10.3f", 4.123456);
+    }
+        
+    }
+}
